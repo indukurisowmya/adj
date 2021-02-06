1b15874c8a5481db50ad9e6d9152d7d1e8b75302
indukurisowmya <78533890+indukurisowmya@users.noreply.github.com>
2021-02-06 19:12:41 +0530

    exp-2

---
+++ exp2/src/exp2/Receiver.java
@@ -0,0 +1,20 @@
+package exp2;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+public class Receiver {
+
+	
+ public static void main(String[] args) throws Exception {
+
+ DatagramSocket ds = new DatagramSocket(300);
+ byte[] buf = new byte[1024];
+
+ DatagramPacket dp = new DatagramPacket(buf, 1024);
+ ds.receive(dp);
+
+ String strRecv = new String(dp.getData(), 0, dp.getLength());
+ System.out.println(strRecv);
+ ds.close();
+ }
+} 
+
---
+++ exp2/src/exp2/Sender.java
@@ -0,0 +1,17 @@
+package exp2;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+public class Sender {
+
+	
+ public static void main(String[] args) throws Exception {
+ DatagramSocket ds = new DatagramSocket();
+ String str = "hello world";
+ InetAddress ia = InetAddress.getByName("localhost");
+ System.out.println(ia);//.toString());
+ DatagramPacket dp = new DatagramPacket(str.getBytes(), str.length(), ia, 300);
+ ds.send(dp);
+ ds.close();
+ }
+}
